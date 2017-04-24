15ca48d4f13ff832fd959818990c7bfdff6f55ca
Calvin Lee <calvin.lee@deakin.edu.au>
2017-04-24 10:56:53 +1000

    Update makeAOOGrid documentation.

--- R/makeAOOGrid.R
+++ R/makeAOOGrid.R
@@ -1,26 +1,30 @@
-#' Compute Area of Occupancy (AOO)
+#' Create Area of Occupancy (AOO) grid for an ecosystem or species distribution
 #'
-#' \code{getAOO} determines the number of area of occupancy (AOO) grid cells
-#' occupied by a species or ecosystem. It includes capability for specifying
-#' the proportion of each grid cell that needs to be occupied before the grid
-#' cell is counted in the AOO. This functionality is important for assessing
-#' the IUCN Red List of Ecosystems Criteria B.
+#' \code{makeAOOGrid} creates grids for species presence based on the presented
+#' raster object. It includes capability for specifying whether at least one
+#' percent of the grid cell needs to be occupied before it is counted in the
+#' AOO. This functionality is important for assessing the IUCN Red List of
+#' Ecosystems Criteria B.
 #'
-#' @param x Raster object of an ecosystem or species distribution
-#' @param grid Raster object of a regular grid suitable for assessing AOO
-#' @return The number of grid cells occupied by the ecosystem or species
-#' @author Nicholas Murray \email{murr.nick@@gmail.com}
+#' @param ecosystem.data Raster object of an ecosystem or species distribution
+#' @param grid.size Integer. A number specifying the width of the desired grid
+#'   square (in same units as your coordiante reference system)
+#' @param one.percent.rule Logical.If \code{TRUE} one percent of the grid cell
+#'   must be occupied before it is counted in the AOO.
+#' @return A shapefile of grid cells occupied by an ecosystem or species
+#' @author Nicholas Murray \email{murr.nick@@gmail.com}, Calvin Lee
 #' @seealso \code{\link{createGrid}}
 #' @references Bland, L.M., Keith, D.A., Miller, R.M., Murray, N.J. and
-#' Rodriguez, J.P. (eds.) 2016. Guidelines for the application of IUCN Red
-#' List of Ecosystems Categories and Criteria, Version 1.0. Gland, Switzerland:
-#' IUCN. ix + 94pp. Available at the following web site: \url{iucnrle.org/}
+#'   Rodriguez, J.P. (eds.) 2016. Guidelines for the application of IUCN Red
+#'   List of Ecosystems Categories and Criteria, Version 1.0. Gland,
+#'   Switzerland: IUCN. ix + 94pp. Available at the following web site:
+#'   \url{iucnrle.org/}
 #' @examples
 #' n = createGrid(r1, 10000)
 #' plot (n)
 #' plot (r1, add = TRUE)
-#' AOO = getAOO(r1, n, one.percent.rule = F)
-#' AOO # number of grid cells occupied by an ecosystem or species
+#' AOO_grid = makeAOOGrid(r1, n, one.percent.rule = F)
+#' AOO_grid # shapefile of grid cells occupied by an ecosystem or species
 
 makeAOOGrid <- function (ecosystem.data, grid.size, one.percent.rule = FALSE) {
   # Computes the number of 10x10km grid cells that are >1% covered by an ecosystem
