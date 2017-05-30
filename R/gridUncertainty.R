8705e2fc4900cc250abfb422e0f054a5e1021183
Calvin Lee <calvin.lee@deakin.edu.au>
2017-05-30 18:23:02 +1000

    Rewrite gridUncertainty
    gridUncertainty now creates different scenarios systematically by sliding a set distance as dictated by the number of splits on a single grid. The output has also been updated to reflect the change, and now reports the amount moved in x and y driection which lead to the grid scenario(s) with the smallest AOO.
    A raster layer of the grids with the smallest AOO is now included within the output.

--- R/gridUncertainty.R
+++ R/gridUncertainty.R
@@ -3,27 +3,39 @@
 #' \code{gridUncertainty} determines the number of area of occupancy (AOO) grid
 #' cells occupied by a species or ecosystem. The key difference with
 #' \code{getAOO} is that it accounts for the impact of the location of the AOO
-#' grid by randomly moving the grid and recomputing AOO at each step.
+#' grid by systematically moving the grid and recomputing AOO at each step.
 #'
 #' @param x Raster object of an ecosystem or species distribution
 #' @param grid Raster object of a regular grid suitable for assessing AOO
-#' @param nSim specifies the number of times to repeat the AOO calculation with
-#'   randomly sampled grid origin coordinates
-#' @return A list of dataframes raw.sims and summary.sims
-#' @author Nicholas Murray \email{murr.nick@@gmail.com}
+#' @param split Specifies the number of ways to split the grid in ONE axis.
+#' @param one.percent.rule Logical. If true, >1% of a grid must be occupied
+#'   before it is counted as an AOO cell
+#' @return List containing vector of length split*split of calculated AOO for
+#'   each shift position, a list of summary statistics for the vector, a list of
+#'   grids which generated the smallest AOO as RasterLayers, and the movements
+#'   which resulted in the grids with the smallest AOO.
+#' @author Nicholas Murray \email{murr.nick@@gmail.com}, Calvin Lee
+#'   \email{calvinkflee@@gmail.com}
 #' @seealso \code{\link{createGrid}} \code{\link{getAOO}}
+#'   \code{\link{getAOOSilent}}
 #' @references Bland, L.M., Keith, D.A., Miller, R.M., Murray, N.J. and
 #'   Rodriguez, J.P. (eds.) 2016. Guidelines for the application of IUCN Red
 #'   List of Ecosystems Categories and Criteria, Version 1.0. Gland,
 #'   Switzerland: IUCN. ix + 94pp. Available at the following web site:
 #'   \url{iucnrle.org/}
 #' @examples
+#' x <- gridUncertainty(r1, 10000, 3, F)
+#' x$AOO.list # vector of length 9 of calculated AOO for each grid position
+#' x$stats # dataframe returning the number of shifts performed, summary statistics
+#' for the calculated AOOs, and the shift values which created the grid
+#' position(s) with the smallest AOO.
+#' x$min.AOO.grid.list # list of RasterLayers of the grids which all return the
+#' minimum AOO.
+#' x$min.AOO.shifts # dataframe containing information of the index of the shift
+#' which created the grids with the smallest AOO and the x and y shifts for them.
 
 
-# TODO: do the examples
-
-gridUncertainty <- function (ecosystem.data, grid.size, nSim = 10, one.percent.rule = FALSE){
-
+gridUncertainty <- function(ecosystem.data, grid.size, split, one.percent.rule = TRUE){
   grid <- createGrid(ecosystem.data, grid.size)
   results.df <- data.frame(sim.no = integer(),
                            x.shift = integer(),
@@ -31,45 +43,47 @@ gridUncertainty <- function (ecosystem.data, grid.size, nSim = 10, one.percent.r
                            dist.move = double(),
                            AOO = integer(),
                            stringsAsFactors = FALSE)
+  intervals <- grid.size/splits
+
+  x.shift <- seq(0, grid.size, (intervals))
+  x.shift <- x.shift[1:length(x.shift)-1] # Removing the final grid which is identical to first
+  y.shift <- seq(0, grid.size, (intervals))
+  y.shift <- y.shift[1:length(y.shift)-1]
+  shift.grid <- expand.grid(x.shift = x.shift, y.shift = y.shift) # Create the movement grid
+
+  grid.shifted.list <- apply(shift.grid, 1, function(gridList){
+    # Pull out the values for each shift
+    current.xshift <- gridList["x.shift"]
+    current.yshift <- gridList["y.shift"]
+    grid.shift <- shift(grid, x = current.xshift, y = current.yshift)
+    return(grid.shift)
+  })
+
+  AOO.list <- sapply(grid.shifted.list, getAOOSilent, # List of AOO for each scenario
+                     ecosystem.data = ecosystem.data, one.percent.rule = one.percent.rule)
+  min.AOO <- min(AOO.list)
+  min.AOO.index <- which(AOO.list==min.AOO)
 
 
-  for (i in 1:nSim){
-
-    message (paste("Starting grid uncertainty number: ",i, "of", nSim))
-    x.shift <- sample(-10000:10000, 1)
-    message (paste("... moved x by:", x.shift))
-    y.shift <- sample(-10000:10000, 1)
-    message (paste("... moved y by:", y.shift))
-    dist.move <- sqrt((x.shift^2)+(y.shift^2))# use pythagoras to work out how far it was moved as a function of a right-angled triangle
-
-    grid.shift <- shift(grid, x=x.shift, y=y.shift) # move the grid
-    AOO <- getAOOSilent (ecosystem.data, grid = grid.shift, one.percent.rule = one.percent.rule) # get the AOO again
+  message(paste("Minimum AOO (", min.AOO, ") is obtained when the grid is shifted in x by",
+                shift.grid[min.AOO.index, 1], "meters and in y by",
+                shift.grid[min.AOO.index, 2], "meters."))
 
 
-    message (paste("... AOO equals:", AOO))
-    sim.df <-  data.frame(sim.no = i,
-                          x.shift = x.shift,
-                          y.shift = y.shift,
-                          dist.move = dist.move,
-                          AOO = AOO,
-                          stringsAsFactors = FALSE)
+  out.df <- data.frame(n.shifts = length(grid.shifted.list),
+                       mean.AOO = mean(AOO.list),
+                       var.AOO = var(AOO.list),
+                       sd.AOO = sd(AOO.list),
+                       min.AOO = min(AOO.list),
+                       max.AOO = max(AOO.list))
 
-    results.df <- rbind(results.df, sim.df)
-  }
-  min.AOO <-  min (results.df$AOO)
-  message (paste("Minimum AOO is from", nSim, "assessments is:", min.AOO, "grid cells"))
-  out.df <- data.frame (n.sims = nSim,
-                        mean.AOO = mean(results.df$AOO),
-                        var.AOO = var(results.df$AOO),
-                        sd.AOO = sd (results.df$AOO),
-                        min.AOO = min (results.df$AOO),
-                        max.AOO = max (results.df$AOO))
-
-  hist.breaks <- seq(min(results.df$AOO),max(results.df$AOO),1)
-  hist(results.df$AOO, breaks = hist.breaks, col = "darkred", main = "Histogram: AOO Uncertainty")
-  return (list(raw.sims = results.df, summary.sims = out.df))
+  hist.breaks <- seq(min(AOO.list), max(AOO.list), 1)
+  hist(AOO.list, breaks = hist.breaks, col = "darkred", main = "Histogram: AOO Uncertainty")
+  return(list(AOO.list = AOO.list, stats = out.df,
+              min.AOO.grid.list = grid.shifted.list[min.AOO.index],
+              min.AOO.shifts = shift.grid[min.AOO.index, ]))
 }
 
 # TODO: change the 1% rule to a number between 0 and 1 specifying how much of the grid cell must be occupied before being selected for AOO
-# TODO: can we get a raster or vector output of the final AOO? Should be possible by passing the grid cell IDs here and selecting the grid cells from the grid object
+
 
