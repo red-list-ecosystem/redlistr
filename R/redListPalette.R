7eb3321f7b3a19c3d5f823dae7066425b5afe9c7
Calvin Lee <calvin.lee@deakin.edu.au>
2017-06-08 18:16:33 +1000

    Update documentation

--- R/redListPalette.R
+++ R/redListPalette.R
@@ -1,14 +1,13 @@
 #' Red List Palette.
 #'
-#'#' \code{redListPalette} creates a character vector of colours suggested by
-#' the IUCN for listing the 8 categories of threat
-
+#' \code{redListPalette} creates a character vector of colours suggested by the
+#' IUCN for listing the 8 categories of threat
 #' @return Returns a character vector of the colours used for each category of
-#' threat by the IUCN
+#'   threat by the IUCN
 #' @author Nicholas Murray \email{murr.nick@@gmail.com}
 #' @examples
 #' pie(rep(1,8), col=redListPalette(), main = "Red List Palette")
-#' n = colorRampPalette(redListPalette())(50)
+#' n <- colorRampPalette(redListPalette())(50)
 #' pie(rep(1,50), col=n, frame = F)
 
 redListPalette <- function(){
@@ -20,7 +19,6 @@ redListPalette <- function(){
   lc <- rgb (0,128/255,0,1)
   dd <- rgb (128/255,128/255,128/255,1)
   ne <- rgb (255/255,255/255,255/255,1)
-  palette = c(co, cr, en, vu, nt, lc, dd, ne)
+  palette <- c(co, cr, en, vu, nt, lc, dd, ne)
   return (palette)
 }
-
