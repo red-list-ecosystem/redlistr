50209685553ade2aea49b0248070ec0fcb1539d4
Calvin Lee <kfle@deakin.edu.au>
2022-09-20 17:20:33 +0800

    Update examples

--- R/AOO_functions.R
+++ R/AOO_functions.R
@@ -54,7 +54,7 @@ createGrid <- function(input.data, grid.size){
 #' crs.UTM55S <- '+proj=utm +zone=55 +south +ellps=WGS84 +datum=WGS84 +units=m +no_defs'
 #' r1 <- raster(ifelse((volcano<130), NA, 1), crs = crs.UTM55S)
 #' extent(r1) <- extent(0, 6100, 0, 8700)
-#' AOO_grid <- makeAOOGrid(r1, 10000, min.percent.rule = TRUE, percent = 1)
+#' AOO_grid <- makeAOOGrid(r1, 1000, min.percent.rule = TRUE, percent = 1)
 #' @export
 #' @import raster
 
