4c073231d631e77c64478be0b0c3e40008bf00ef
Calvin Lee <kfle@deakin.edu.au>
2022-09-20 17:19:52 +0800

    Update examples
    Updating examples to make more sense

--- R/AOO_functions.R
+++ R/AOO_functions.R
@@ -137,7 +137,7 @@ makeAOOGrid.SpatialPolygons <-
 #' crs.UTM55S <- '+proj=utm +zone=55 +south +ellps=WGS84 +datum=WGS84 +units=m +no_defs'
 #' r1 <- raster(ifelse((volcano<130), NA, 1), crs = crs.UTM55S)
 #' extent(r1) <- extent(0, 6100, 0, 8700)
-#' AOO <- getAOO(r1, 10000, min.percent.rule = TRUE, percent = 1)
+#' AOO <- getAOO(r1, 1000, min.percent.rule = TRUE, percent = 1)
 #' @export
 
 getAOO <- function(input.data, grid.size, min.percent.rule = FALSE, percent = 1){
