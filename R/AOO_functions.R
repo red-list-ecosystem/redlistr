4f54b18b9d8586aa1e87732e3ece4d09ad35004d
Calvin Lee <kfle@deakin.edu.au>
2022-09-20 17:07:13 +0800

    Update gridUncertainty example
    Previous example parameters were unrealistic. Changed to fit the input.

--- R/AOO_functions.R
+++ R/AOO_functions.R
@@ -222,7 +222,7 @@ getAOOSilent.SpatialPoints <-
       area <- cell.res[1] * cell.res[2]
       one.pc.grid <- grid.size * grid.size / 100 # 1pc of grid cell
       threshold <- one.pc.grid * percent / area
-      outGrid <- grid.shp[grid.shp$count > threshold, ] # select only grids that meet one percent threshol
+      outGrid <- grid.shp[grid.shp$count > threshold, ] # select only grids that meet one percent threshold
     }
 
     # end getAOO
--- R/gridUncertainty_functions.R
+++ R/gridUncertainty_functions.R
@@ -192,7 +192,7 @@ gridUncertaintyRandomManual <- function(input.data, grid.size, n.sim = 10,
 #' crs.UTM55S <- '+proj=utm +zone=55 +south +ellps=WGS84 +datum=WGS84 +units=m +no_defs'
 #' r1 <- raster(ifelse((volcano<130), NA, 1), crs = crs.UTM55S)
 #' extent(r1) <- extent(0, 6100, 0, 8700)
-#' x <- gridUncertaintyRandom(r1, grid.size = 10000, n.AOO.improvement = 50,
+#' x <- gridUncertaintyRandom(r1, grid.size = 1000, n.AOO.improvement = 50,
 #'                            min.percent.rule = TRUE, percent = 1)
 #' @export
 #' @import raster
@@ -291,7 +291,7 @@ gridUncertaintyRandom <- function(input.data, grid.size, n.AOO.improvement,
 #' crs.UTM55S <- '+proj=utm +zone=55 +south +ellps=WGS84 +datum=WGS84 +units=m +no_defs'
 #' r1 <- raster(ifelse((volcano<130), NA, 1), crs = crs.UTM55S)
 #' extent(r1) <- extent(0, 6100, 0, 8700)
-#' x <- gridUncertainty(r1, grid.size = 10000, n.AOO.improvement = 5,
+#' x <- gridUncertainty(r1, grid.size = 1000, n.AOO.improvement = 5,
 #'                            min.percent.rule = FALSE, percent = 1)
 #' @export
 
