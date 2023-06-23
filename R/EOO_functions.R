40a80fbbc432bbee0951a4b8dd88d7df8b8ead6e
Calvin Lee <kfle@deakin.edu.au>
2023-06-23 15:46:58 +0800

    update getAreaEOO
    The function now accepts SpatVect as expected, and uses terra::expanse to calculate area.

--- R/EOO_functions.R
+++ R/EOO_functions.R
@@ -76,24 +76,22 @@ makeEOO.SpatVector <- function(input.data){
 #'
 #' \code{getAreaEOO} calculates the area of the EOO polygon generated from
 #' \code{makeEOO} the provided data
-#' @param EOO.polygon An object of class SpatialPolygons, usually the output
+#' @param EOO.polygon An object of class SpatVect, usually the output
 #'   from \code{makeEOO}.
+#' @param unit Character. Output unit of area. One of "m", "km", or "ha"
 #' @return The area of the \code{EOO.polygon} in km2
 #' @author Nicholas Murray \email{murr.nick@@gmail.com}, Calvin Lee
 #'   \email{calvinkflee@@gmail.com}
 #' @family EOO functions
 #' @examples
 #' crs.UTM55S <- '+proj=utm +zone=55 +south +ellps=WGS84 +datum=WGS84 +units=m +no_defs'
-#' r1 <- raster(ifelse((volcano<130), NA, 1), crs = crs.UTM55S)
+#' r1 <- rast(ifelse((volcano<130), NA, 1), crs = crs.UTM55S)
 #' extent(r1) <- extent(0, 6100, 0, 8700)
 #' EOO.polygon <- makeEOO(r1)
 #' EOO.area <- getAreaEOO(EOO.polygon)
 #' @export
 
-getAreaEOO <- function(EOO.polygon){
-  # Returns the area of the makeEOO output (spatialpolygons object)
-  EOO.aream2 <- sapply(methods::slot(EOO.polygon, "polygons"), methods::slot, "area")
-  # get the area from the slots in the polygon dataset
-  EOO.areakm2 <- EOO.aream2/1000000
-  return(EOO.areakm2)
+getAreaEOO <- function(EOO.polygon, unit = "km"){
+  EOO.area <- expanse(EOO.polygon, unit)
+  return(EOO.area)
 }
