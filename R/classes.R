5a61973352ec700872b210033687019633d8214e
Anikó B. Tóth <anikobtoth@users.noreply.github.com>
2025-11-14 11:51:46 +1100

    Add new EOO class objects and convenience functions

--- R/classes.R
+++ R/classes.R
@@ -101,3 +101,97 @@ setMethod(
 
   }
 )
+
+
+#' EOO class
+#'
+#' A class to represent an EOO convex hull object
+#'
+#' @slot pol an EOO convex hull polygon
+#' @slot EOO the area of the EOO polygon as a numeric
+#' @slot unit the unit in which the area is provided
+#' @slot input the input ecosystem data used to generate the EOO polygon
+#' @export
+setClass(
+  "EOO",
+  slots = list(
+    pol =     "sf",
+    EOO =     "numeric",
+    unit =    "character",
+    input =   "geospatial"
+  ),
+  prototype = list(
+    pol = NULL,
+    EOO = numeric(0),
+    unit = character(0),
+    input = NULL
+  )
+)
+
+
+#' @export
+setMethod("show", "EOO", function(object) {
+  cat("Class 'EOO'\n")
+  cat("  EOO area:", format(object@EOO, big.mark = ","), object@unit , "\n", sep = " ")
+  cat("  Polygon geometry type: ", paste(unique(sf::st_geometry_type(object@pol)), collapse = ", "), "\n", sep = "")
+  cat("  Input object class: ", class(object@input)[1], "\n", sep = "")
+})
+
+#' @export
+setMethod("summary", "EOO", function(object, ...) {
+  cat("Summary of EOO object\n")
+  cat("----------------------------\n")
+  cat("EOO area: ", format(object@EOO, big.mark = ","), " square units\n", sep = "")
+  cat("Polygon geometry type(s): ", paste(unique(sf::st_geometry_type(object@pol)), collapse = ", "), "\n", sep = "")
+  cat("Number of polygon features: ", nrow(object@pol), "\n", sep = "")
+  cat("Input data class: ", class(object@input)[1], "\n", sep = "")
+
+  if (inherits(object@input, "sf")) {
+    cat("Input feature count: ", nrow(object@input), "\n", sep = "")
+  } else if (inherits(object@input, "SpatRaster")) {
+    cat("Input raster layers: ", terra::nlyr(object@input), "\n", sep = "")
+    cat("Raster dimensions: ", paste(terra::ext(object@input), collapse = " × "), "\n", sep = "")
+  }
+  invisible(object)
+})
+
+#' @export
+setMethod("plot", "EOO", function(x, y, ...) {
+  if (is.null(x@pol) || nrow(x@pol) == 0) {
+    stop("No polygon data found in slot 'pol'.")
+  }
+  if (is.null(x@input)) {
+    stop("No input data found in slot 'input'.")
+  }
+
+  # Set up combined extent if possible
+  if (inherits(x@input, "sf")) {
+    bbox_all <- sf::st_bbox(sf::st_union(sf::st_geometry(x@pol), sf::st_geometry(x@input)))
+  } else if (inherits(x@input, "SpatRaster")) {
+    bbox_all <- sf::st_bbox(sf::st_as_sf(terra::as.polygons(x@input)))
+  } else {
+    stop("Unsupported input type in slot 'input'. Must be 'sf' or 'SpatRaster'.")
+  }
+
+  # --- Base plot ---
+  plot(sf::st_geometry(x@pol),
+       border = "gray12", col = scales::alpha("gray30", 0.2),
+       main = "EOO polygon and input data",
+       xlim = bbox_all[c("xmin", "xmax")],
+       ylim = bbox_all[c("ymin", "ymax")],
+       axes = TRUE, ...)
+
+  # --- Overlay input ---
+  if (inherits(x@input, "sf")) {
+    plot(sf::st_geometry(x@input),
+         pch = 21, col = "black", bg = "blue",
+         add = TRUE)
+  } else if (inherits(x@input, "SpatRaster")) {
+    terra::plot(x@input, add = TRUE, col = c("gray45", "midnightblue"))
+  }
+
+  box()
+})
+
+
+
