15fcd7ac0240211c1f10c5bc7004b8a9422b1446
José R. Ferrer Paris <jr.ferrer.paris@gmail.com>
2025-12-10 11:48:34 +1100

    Update makeEOO.sf to use input.data instead of pols #21
    Quick fix

--- R/EOO_functions.R
+++ R/EOO_functions.R
@@ -44,8 +44,8 @@ makeEOO.SpatRaster <- function(input.data){
 #' @export
 makeEOO.sf <- function(input.data, names_from = NA){
   names_from <- coalesce(names_from, "ecosystem_name")
-  if (!any(colnames(pols) %in% names_from)) {
-    pols <- pols |> dplyr::mutate(ecosystem_name = "unnamed ecosystem type")
+  if (!any(colnames(input.data) %in% names_from)) {
+    input.data <- input.data |> dplyr::mutate(ecosystem_name = "unnamed ecosystem type")
   }
  input.split <- split(input.data, st_drop_geometry(input.data[names_from]))
  EOO.polygon <- input.split |> lapply(st_union) |> lapply(st_convex_hull) |> lapply(st_sf)
