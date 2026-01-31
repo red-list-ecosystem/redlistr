12c7dc77f8258b3453fde448b9dcb3a91e65a8e6
Anikó B. Tóth <anikobtoth@users.noreply.github.com>
2026-01-31 14:45:13 +1100

    tests and vignettes updated

--- tests/testthat/test-AOO_functions.R
+++ tests/testthat/test-AOO_functions.R
@@ -1,63 +1,38 @@
 test_that("accepts different format", {
   set.seed(1)
 
-  # SpatialPoints
+  # sf POINTS
   pts = cbind(1:5, 1:5)
   dimnames(pts)[[1]] = letters[1:5]
-  df = data.frame(a = 1:5)
-  row.names(df) = letters[5:1]
-  sp_points <- SpatialPoints(pts)
-  crs(sp_points) <- "+proj=utm +zone=55 +south +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"
+  pts = data.frame(pts)
+  sp_points <- st_as_sf(pts, coords = c("X1", "X2"), crs = 32755)
 
-  expect_equal(getAOO(sp_points, 1), 5)
+  expect_equal(getAOO(sp_points, 1)@AOO, 5)
 
-  #SpatialPointsDataFrame
-  sp_points_df <- SpatialPointsDataFrame(pts, df, match.ID = TRUE)
-  crs(sp_points_df) <- "+proj=utm +zone=55 +south +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"
+  #sf POLYGONS
+  poly <- list(
+    st_polygon(list(cbind(c(2,4,4,1,2),c(2,3,5,4,2)))),
+    st_polygon(list(cbind(c(5,4,2,5),c(2,3,2,2)))),
+    st_polygon(list(cbind(c(4,4,5,10,4),c(5,3,2,5,5)))))
 
-  expect_equal(getAOO(sp_points_df, 1), 5)
+  poly_sf <- st_sf(id = 1:length(poly), geometry = st_sfc(poly), crs = 32755)
 
-  #SpatialPolygons
-  Sr1 <- Polygon(cbind(c(2,4,4,1,2),c(2,3,5,4,2)))
-  Sr2 <- Polygon(cbind(c(5,4,2,5),c(2,3,2,2)))
-  Sr3 <- Polygon(cbind(c(4,4,5,10,4),c(5,3,2,5,5)))
-  Srs1 <- Polygons(list(Sr1), "s1")
-  Srs2 <- Polygons(list(Sr2), "s2")
-  sp_polygons <- SpatialPolygons(list(Srs1,Srs2), 1:2)
-  crs(sp_polygons) <- "+proj=utm +zone=55 +south +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"
-
-  expect_equal(getAOO(sp_polygons, 1), 10)
-
-  #SpatialPolygonsDataframe
-  grd <- GridTopology(c(1,1), c(1,1), c(10,10))
-  polys <- as(grd, "SpatialPolygons")
-  centroids <- coordinates(polys)
-  x <- centroids[,1]
-  y <- centroids[,2]
-  z <- 1.4 + 0.1*x + 0.2*y + 0.002*x*x
-  sp_polygons_df <- SpatialPolygonsDataFrame(polys,
-                                             data=data.frame(x=x, y=y, z=z,
-                                                             row.names=row.names(polys)))
-  crs(sp_polygons_df) <- "+proj=utm +zone=55 +south +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"
-
-  expect_equal(getAOO(sp_polygons_df, 1), 100)
+  expect_equal(getAOO(poly_sf, 1, jitter = F)@AOO, 21)
 
   #Raster
-  r <- raster(nrows=10, ncols=10)
+  r <- rast(nrows=10, ncols=10, crs = "epsg:32755")
   values(r) <- 1
-  crs(r) <- "+proj=utm +zone=55 +south +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"
 
-  expect_equal(getAOO(r, 100), 8)
+
+  expect_equal(getAOO(r, 100)@AOO, 8)
 })
 
 
-test_that("minimum percent rule fails when using spatial points", {
+test_that("minimum percent rule fails when using geocentric coordinates", {
   pts = cbind(1:5, 1:5)
   dimnames(pts)[[1]] = letters[1:5]
-  df = data.frame(a = 1:5)
-  row.names(df) = letters[5:1]
-  sp_points <- SpatialPoints(pts)
-  crs(sp_points) <- "+proj=utm +zone=55 +south +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"
+  pts = data.frame(pts)
+  sp_points <- st_as_sf(pts, coords = c("X1", "X2"), crs = 4326)
 
-  expect_error(getAOO(sp_points, 1, T))
+  expect_error(getAOO(sp_points, 1))
 })
